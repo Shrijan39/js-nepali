d6e5abb8e0fbe8c922a189842af0edec683a0908
Shrijan39 <144520070+Shrijan39@users.noreply.github.com>
2023-12-25 08:03:35 +0000

    haha

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS Code as a new version
+
+//alert(3+3)// we are using node js not browser
+console.log(3+3) //code relatibility should be high
+console.log("shrijan")
+
+
+let name = "Shrijan" //string data types
+let age = 14 //number data types
+let isLoggedIn = true //Boolean type
+let unset; //undefined
+
+//number => 2 power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
